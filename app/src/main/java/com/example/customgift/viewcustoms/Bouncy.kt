890dc5086f508c7638db2f0787b46ee858183f3d
doanvu2000 <doanvu20122000@gmail.com>
2021-12-27 23:49:53 +0700

    fix color

--- app/src/main/java/com/example/customgift/viewcustoms/Bouncy.kt
+++ app/src/main/java/com/example/customgift/viewcustoms/Bouncy.kt
@@ -19,8 +19,9 @@ class Bouncy : GridLayout {
 
     companion object {
         const val TIME_CHAIN_STEP_DELAY = 100L
-        const val DEFAULT_CIRCLES_COUNT = 5
-        val listColor = mutableListOf(Color.BLUE, Color.GREEN, Color.BLACK, Color.GRAY, Color.RED, Color.YELLOW)
+        const val DEFAULT_COUNT = 5
+        val listColor =
+            mutableListOf(Color.BLUE, Color.GREEN, Color.BLACK, Color.GRAY, Color.RED, Color.YELLOW, Color.LTGRAY)
         var count = 0
     }
 
@@ -32,20 +33,18 @@ class Bouncy : GridLayout {
             R.styleable.Bouncy,
             0, 0
         ).apply {
-            try {
-                circlesCount = getInt(R.styleable.Bouncy_circlesCount, DEFAULT_CIRCLES_COUNT)
-                columnCount = circlesCount
-                rowCount = 1
-            } finally {
-                recycle()
-            }
+            circlesCount = getInt(R.styleable.Bouncy_circlesCount, DEFAULT_COUNT)
+            columnCount = circlesCount
+            rowCount = 1
+            recycle()
         }
         initCircles(circlesCount)
     }
 
     private fun initCircles(circleCounts: Int) {
         for (i in 0 until circleCounts) {
-            val circle = Circle(i, circleCounts, getRandomColor(), context)
+            listColor.shuffle()
+            val circle = Circle(i, circleCounts, listColor[0], context)
             addView(circle)
             listCircles.add(circle)
         }
@@ -55,28 +54,25 @@ class Bouncy : GridLayout {
         val handler2 = Handler(Looper.getMainLooper())
         val top = 500
         val bottom = 1500
-        val run = object :Runnable{
+        val run = object : Runnable {
             override fun run() {
                 handler2.postDelayed({
                     listCircles.forEach { circle ->
                         startChainBouncy(circle, top)
                     }
 //                    startChainBouncy(listCircles[1], top)
-                },1000)
+                }, 1000)
                 handler2.postDelayed({
                     listCircles.forEach { circle ->
                         startChainBouncy(circle, bottom)
                     }
 //                    startChainBouncy(listCircles[1], bottom)
-                },500)
-                postDelayed(this,1500)
+                }, 500)
+                postDelayed(this, 1500)
             }
         }
-        handler.postDelayed(run,1500)
-        startChainBouncy(listCircles[1],top)
-        startChainBouncy(listCircles[1],bottom)
-        Log.d("TAG", "initCircles: $count")
-        //drag circle
+        handler.postDelayed(run, 1500)
+        //drag first circle
         listCircles[0].setCallback { pos ->
             listCircles.forEach { circle ->
                 startChainBouncy(circle, pos)
@@ -85,19 +81,11 @@ class Bouncy : GridLayout {
         }
     }
 
-    private fun startChainBouncy(circle: Circle, newY: Int) {
+    private fun startChainBouncy(circle: Circle, pos: Int) {
         CoroutineScope(Dispatchers.Default).launch {
             delay(TIME_CHAIN_STEP_DELAY * circle.pos)
-            circle.moveCircles(newY)
-//            if (circle.pos > 0) {
-//                circle.moveCircles(newY)
-//            }
+            circle.moveCircles(pos)
         }
-    }
-
-    private fun getRandomColor(): Int {
-        listColor.shuffle()
-        return listColor[0]
     }
 
 }
--- app/src/main/java/com/example/customgift/viewcustoms/Circle.kt
+++ app/src/main/java/com/example/customgift/viewcustoms/Circle.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.util.Log
 import android.view.KeyEvent.ACTION_DOWN
 import android.view.KeyEvent.ACTION_UP
 import android.view.MotionEvent
@@ -17,16 +16,30 @@ import kotlin.math.abs
 class Circle(
     val pos: Int = 0,
     private val numCircles: Int = 5,
-    private val colorCircle: Int = Color.GREEN,
+    private val colorCircle: Int = Color.RED,
     context: Context
 ) :
     View(context) {
-    private var mX: Int = 0
-    var mY: Int = 0
 
-    private val speed = DEFAULT_SPEED
+    private var x: Int = 0
+    var mY: Int = 0
+    private var job: Job? = null
+    private val speed = 20
+    private var radius: Float = 0F
+    private var initCoordinatesX = 0
+    private var initCoordinatesY = 0
 
     private var callBack: ((Int) -> Unit)? = null
+
+    fun setCallback(action: (Int) -> Unit) {
+        callBack = action
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        drawCircle(canvas)
+    }
+
     private val mPaintCircle = Paint().apply {
         color = colorCircle
         style = Paint.Style.FILL_AND_STROKE
@@ -34,35 +47,29 @@ class Circle(
         strokeCap = Paint.Cap.ROUND
         isAntiAlias = true
     }
-    private var radius: Float = 0F
-    private var initCoordinatesX = 0
-    private var initCoordinatesY = 0
+
+    private fun drawCircle(canvas: Canvas?) {
+        canvas?.drawCircle(x.toFloat(), mY.toFloat(), radius, mPaintCircle)
+        invalidate()
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val mWidth = MeasureSpec.getSize(widthMeasureSpec) / numCircles
         val mHeight = MeasureSpec.getSize(heightMeasureSpec)
         initCoordinatesY = mHeight / 2
         initCoordinatesX = mWidth / 2
         radius = mWidth / 5F
-        mX = initCoordinatesX
+        x = initCoordinatesX
         mY = initCoordinatesY
         setMeasuredDimension(mWidth, mHeight)
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        drawCircle(canvas)
-    }
-
-    private fun drawCircle(canvas: Canvas?) {
-        canvas?.drawCircle(mX.toFloat(), mY.toFloat(), radius, mPaintCircle)
-        invalidate()
-    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (pos == 0) {
             when (event?.action) {
                 ACTION_DOWN -> {
-                    Log.e("size", event.y.toString())
                     job?.cancel()
                 }
 
@@ -73,7 +80,7 @@ class Circle(
                 }
 
                 ACTION_UP -> {
-                    reset(event.y.toInt())
+                    clear(event.y.toInt())
                 }
             }
             return true
@@ -82,32 +89,23 @@ class Circle(
         }
     }
 
-    private var job: Job? = null
-    private fun reset(y: Int) {
+
+    private fun clear(y: Int) {
         val direction = if (y > initCoordinatesY) -1 else 1
         job = CoroutineScope(Dispatchers.Default).launch {
-            delay(TIME_RESET_DELAY)
+            delay(5000L)
             while (abs(mY - initCoordinatesY) >= speed) {
                 mY += speed * direction
                 callBack?.invoke(mY)
-                delay(TIME_STEP_DELAY)
+                delay(10L)
             }
             mY += abs(mY - initCoordinatesY) * direction
             callBack?.invoke(mY)
         }
     }
 
-    fun setCallback(action: (Int) -> Unit) {
-        callBack = action
+    fun moveCircles(y: Int) {
+        mY = y
     }
 
-    fun moveCircles(newPos: Int) {
-        mY = newPos
-    }
-
-    companion object {
-        const val TIME_RESET_DELAY = 3000L
-        const val TIME_STEP_DELAY = 10L
-        const val DEFAULT_SPEED = 20
-    }
 }
